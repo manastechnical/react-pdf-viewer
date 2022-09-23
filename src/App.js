e72398a8b3eeabce4f25fe67685b9acc2ec7720e
Manas <manasjagtap20@gmail.com>
2022-09-23 18:18:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
+  const [pdfFile, setPdfFile] = useState(null);
+
+  const [pdfError, setPdfError] = useState('');
+
+
+  const allowedFiles = ['application/pdf'];
+  const handleFile = (e) => {
+    let selectedFile = e.target.files[0];
+    console.log(selectedFile);
+    if (selectedFile) {
+      if (selectedFile && allowedFiles.includes(selectedFile.type)) {
+        let reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend = (e) => {
+          setPdfError('');
+          setPdfFile(e.target.result);
+        }
+      }
+      else {
+        setPdfError('Not a valid pdf: Please select only PDF');
+        setPdfFile('');
+      }
+    }
+    else {
+      console.log('please select a PDF');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form>
+        <label><h5>Upload PDF</h5></label>
+        <br></br>
+
+        <input type='file' className="form-control"
+          onChange={handleFile}></input>
+
+        {pdfError && <span className='text-danger'>{pdfError}</span>}
+
+      </form>
+
+      <h5>View PDF</h5>
+      <div className="viewer">
+        {pdfFile && (
+          // worker version & pdfjs-dist version must be same. In this case version is 2.15.349
+          <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
+            <Viewer fileUrl={pdfFile}
+              plugins={[defaultLayoutPluginInstance]}></Viewer>
+          </Worker>
+        )}
+
+        {/* render this if we have pdfFile state null   */}
+        {!pdfFile && <>No file is selected yet</>}
+
+      </div>
+
     </div>
   );
 }
+
 
 export default App;
